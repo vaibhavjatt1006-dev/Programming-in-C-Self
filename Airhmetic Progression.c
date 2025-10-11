a6cb09738ca281d7384a63c2c17c18ee1fac06eb
vaibhavjatt1006-dev <vaibhavjatt1006@gmail.com>
2025-10-11 22:49:04 +0530

    Add files via upload

---
+++ Airhmetic Progression.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main () 
+{
+int n;
+printf("Enter number of terms in A.P : ");
+scanf("%d",&n);
+int a;
+printf("Enter first term of A.P : ");
+scanf("%d",&a);
+int d;
+printf("Enter common difference in A.P : ");
+scanf("%d",&d);
+printf("A.P is :\n");
+for(int i=1;i<=10;i++)
+{
+printf("%d\n",a);
+a=a+d;
+}
+return 0;
+}
