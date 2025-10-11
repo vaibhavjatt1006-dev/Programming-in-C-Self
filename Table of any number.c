ee0b26093ea257af905ca6daa34b0edfa797548a
vaibhavjatt1006-dev <vaibhavjatt1006@gmail.com>
2025-10-11 22:31:09 +0530

    Update Table of any number.c

--- Table of any number.c
+++ Table of any number.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 int main() { 
 int n;
-printf("Enter no. :");
+printf("Enter positive number :");
 scanf("%d",&n);
+printf("Table of %d is :\n",n);
 for(int i=1;i<=10;i=i+1) {
 printf("%dx%d=%d\n",n,i,i*n);
 }
