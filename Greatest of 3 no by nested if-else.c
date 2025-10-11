08b9843ce904a2fb9f5cef36c6289d594f0f1ae9
vaibhavjatt1006-dev <vaibhavjatt1006@gmail.com>
2025-10-11 16:01:13 +0530

    Add files via upload

---
+++ Greatest of 3 no by nested if-else.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main () 
+{
+int a,b,c;
+printf("Enter first number :");
+scanf("%d",&a);
+printf("Enter second number :");
+scanf("%d",&b);
+printf("Enter third number :");
+scanf("%d",&c);
+if (a>b)
+{
+if(a>c)
+{
+printf("%d is greatest",a);
+}
+else
+{
+printf("%d is greatest",c);
+}
+}
+else
+{
+if(b>c)
+{
+printf("%d is greatest",b);
+}
+else
+{
+printf("%d is greatest",c);
+}
+}
+return 0;
+}
