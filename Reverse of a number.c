51f04d69a92d13dc54de02bd4a02deb1eb3b324f
vaibhavjatt1006-dev <vaibhavjatt1006@gmail.com>
2025-10-13 12:45:22 +0530

    Add files via upload

---
+++ Reverse of a number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main () 
+{
+int n;
+printf("Enter any digit number : ");
+scanf("%d",&n);
+int m=n;
+int rev=0;
+while (n>0)
+{
+int d=n%10;
+rev=rev*10+d;
+n=n/10;
+}
+printf("The reverse of %d is : %d",m,rev);
+return 0;
+}
