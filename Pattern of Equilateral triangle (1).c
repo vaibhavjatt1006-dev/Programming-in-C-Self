28dd6941308869e4f69dfe685a8d1b592719243e
vaibhavjatt1006-dev <vaibhavjatt1006@gmail.com>
2025-10-19 21:02:42 +0530

    Add files via upload

---
+++ Pattern of Equilateral triangle (1).c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int r,nos=1;
+    printf("Enter number of rows : ");
+    scanf("%d",&r);
+    for (int i=1; i<=r; i++)
+    {
+        for (int j=1; j<=r-i; j++)
+        {
+            printf(" ");
+        }
+        for (int k=1; k<=nos; k++)
+        {
+            printf("* ");
+        }
+        nos = nos + 1;
+        printf("\n");
+    }
+    return 0;
+}
